503af72b505166eb50d269f464089babdf486f46
Federica Spoto <44530686+fdspoto@users.noreply.github.com>
2025-06-02 10:18:54 -0400

    Update 05_county_similarity.R

--- analysis/05_county_similarity.R
+++ analysis/05_county_similarity.R
@@ -54,7 +54,6 @@ ADRD_hospitalization = ADRD_hospitalization[ADRD_hospitalization$county %in% val
 # Get ADRD codes 
 dementia_codes <- read.csv("../data/ADRD_ICD10.csv", stringsAsFactors = FALSE)
 names(dementia_codes) <- c("ICD10", "category", "Description")
-dementia_codes$ICD10[1] = "F0150"
 ADRD_codes = dementia_codes$ICD10
 
 dementia_groups = dementia_codes %>%
